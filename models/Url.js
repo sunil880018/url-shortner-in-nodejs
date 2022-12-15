57e25fa8c4494579992ff2294b3c20645b4fda00
Sunil Kumar <sunil880018@gmail.com>
2022-12-15 17:48:42 +0530

    Update Url.js

--- models/Url.js
+++ models/Url.js
@@ -4,12 +4,19 @@ const URLSchema = new Schema(
     short_url: {
       type: String,
       required:true,
+      minLength: 7,
+      maxLength:25,
       trim: true,
     },
     long_url: {
       type: String,
       required:true,
       trim: true,
+    },
+    createdAt: { // remove this document after 10 minutes
+      type: Date,
+      default: Date.now,
+      expires: 10*60, // 10 minutes
     },
   },
   { timestamps: true } //to include createdAt and updatedAt
